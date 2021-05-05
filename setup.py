f71af7dbb2326de2553b0c746ae57815b4282d37
Wojtek Wilimowski <wwilim@softserveinc.com>
2021-05-05 12:56:03 +0200

    Setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='markdownista',
+    version='',
+    packages=['markdownista'],
+    url='',
+    license='',
+    author='wwilim',
+    author_email='',
+    description=''
+)
